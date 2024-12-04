71e301c52a5eea5e9ae26de81a8dec3d45cb716f
srtushar99 <tushar.bhaliya@swiftrut.com>
2024-12-05 02:38:04 +0530

    Some code update

--- frontend/src/components/Resident Management/OwnerPage.jsx
+++ frontend/src/components/Resident Management/OwnerPage.jsx
@@ -149,6 +149,49 @@ const OwnerPage = () => {
     console.log("Form submitted with :", formData);
   };
 
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (isFormValid) {
+  //     try {
+  //       const formData = new FormData();
+  //       formData.append("Full_name", formData.fullName);
+  //       formData.append("Phone_number", MailOrPhone);
+  //       formData.append("Email_address", moment(shiftDate).format('DD/MM/YYYY'));
+  //       formData.append("Age", time);
+  //       formData.append("Gender", shift);
+  //       formData.append("Wing", gender);
+  //       formData.append("Unit", formData.fullName);
+  //       formData.append("Relation", MailOrPhone);
+  //       formData.append("Email_address", moment(shiftDate).format('DD/MM/YYYY'));
+  //       formData.append("Age", time);
+  //       formData.append("Gender", shift);
+  //       formData.append("Wing", gender);
+
+
+  //       if (uploadprofileimage) {
+  //         formData.append("profileimage", uploadprofileimage); 
+  //       }
+  //       if (adharcard) {
+  //         formData.append("adhar_card", adharcard); 
+  //       }
+
+  //       const response = await axiosInstance.post("/v2/security/addsecurity", formData, {
+  //         headers: { "Content-Type": "multipart/form-data" },
+  //       });
+  
+  //       if (response.status === 200) {
+  //         fetchSecurityGuard();
+  //         onClose();
+  //         ClearAllData();
+  //       }
+  //     } catch (error) {
+  //       console.error("Error creating Guard:", error.response || error.message);
+  //     }
+  //   } else {
+  //     console.log("Form is invalid");
+  //   }
+  // };
+
   const handleVehicleDetailChange = (index, name, value) => {
     // Validation for vehicle details
     if (name === "vehicleName") {
